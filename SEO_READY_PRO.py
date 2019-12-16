20e8d49d86e761113fafe65e6b37de6150db1dd5
lmacchiavelli <lmacchiavelli@gmail.com>
2019-12-16 10:03:51 +0100

    Update

--- SEO_READY_PRO.py
+++ SEO_READY_PRO.py
@@ -4,15 +4,34 @@ gruppi = [{'id': 1, 'name': 'uno', 'keyword': ['01', '02', '03']}, {'id': 2, 'na
 
 count = 0
 header = ""
+line = ""
+column = ""
+modulo = len(gruppi)
+
+
 for i in gruppi:
 	if count == 0:
 		header = "| " + i["name"] + " | "
+		line = "|:--"
 	else:
 		header += i["name"] + " |"
+		line += "|:--|"
 	count+= 1
 
-print(header)
 
+
+
+# linecount = 0
+# for i in gruppi:
+# 	print(i["name"])
+# 	for x in i["keyword"]:
+# 		column +
+# 		print(x)
+# 		linecount+=1
+
+
+comp = (header+"\n"+ line + "\n" + column)
+print (modulo)
 # print( "|" + i["name"] + "|")
 # 	for x in i["keyword"]:
 # 		print(x)
