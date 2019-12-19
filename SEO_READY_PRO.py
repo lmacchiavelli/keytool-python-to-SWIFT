cb5074eee5ef9ef1d29a1a2e8128878f91ff4b6f
lmacchiavelli <lmacchiavelli@gmail.com>
2019-12-19 11:56:07 +0100

    Update SEO_READY_PRO.py

--- SEO_READY_PRO.py
+++ SEO_READY_PRO.py
@@ -1,7 +1,31 @@
-gruppi = [{'id': 1, 'name': 'uno', 'keyword': ['01', '02', '03']}, {'id': 2, 'name': 'due', 'keyword': ['01', '02', '03']}]
+import numpy as np
+
+gruppi = [
+	{'id': 1, 'name': 'uno', 'keyword': ['01', '02', '03','05','06','07']},
+	{'id': 2, 'name': 'due', 'keyword': ['01', '02', '03']},
+	{'id': 2, 'name': 'tre', 'keyword': ['01', '02', '03','04']},
+	{'id': 2, 'name': 'quattro', 'keyword': ['01', '02', '03']},
+]
+
+biggroup = 0
 
 
+for i in gruppi:
+	if len(i["keyword"]) > biggroup:
+		biggroup = len(i["keyword"])
+	
 
+a = np.zeros((biggroup,len(gruppi)))
+
+a[0][1].add("ciao")
+
+for i in a:
+	print(str(i)+"cc")
+	for x in i:
+		print(str(x)+"sss")
+
+
+print(a)
 count = 0
 header = ""
 line = ""
@@ -10,6 +34,14 @@ modulo = len(gruppi)
 
 
 for i in gruppi:
+	mod = count % 2
+	colline = ''
+
+	# if(mod==0):
+	# 	colline = "| " +  
+	# 	print("pari" + str(mod))
+	# else:
+
 	if count == 0:
 		header = "| " + i["name"] + " | "
 		line = "|:--"
@@ -18,6 +50,7 @@ for i in gruppi:
 		line += "|:--|"
 	count+= 1
 
+print(header)
 
 
 
